e5e99e5806ea367e0510ea06526b17f07249ad3f
Milan Petkovic <milan96petkovic@gmail.com>
2022-10-06 04:50:32 +0200

    1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
+    name='anthem-utils',
     version='0.0.1',
     author='Mike Huls',
     author_email='mike_huls@hotmail.com',
