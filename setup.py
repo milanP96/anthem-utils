634d2e1c3f4255b1dc540d10d068af68d85952d4
Milan Petkovic <milan96petkovic@gmail.com>
2022-10-06 04:49:01 +0200

    1

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
     },
     license='MIT',
-    packages=['anthem-utils'],
+    packages=[],
     install_requires=['requests'],
 )
